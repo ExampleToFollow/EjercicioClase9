4b5b138292c6c1c393b249f94328658b9b5b6a01
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-26 17:01:15 -0500

    Agregando cosas

---
+++ src/main/java/com/example/ejercicio6/Controller/WSController.java
@@ -0,0 +1,12 @@
+package com.example.ejercicio6.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ws")
+public class WSController {
+    //Crear rest server
+
+
+}
