492a46bfd0bc990bf8aad5ed838b536f4fbe6170
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-27 00:57:21 -0500

    Hola

--- src/main/java/com/example/ejercicio6/Controller/WSController.java
+++ src/main/java/com/example/ejercicio6/Controller/WSController.java
@@ -2,13 +2,14 @@ package com.example.ejercicio6.Controller;
 
 import com.example.ejercicio6.Entity.Character;
 import com.example.ejercicio6.Repository.CharacterRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 public class WSController {
@@ -18,27 +19,62 @@ public class WSController {
     public WSController(CharacterRepository characterRepository){
         this.characterRepository = characterRepository;
     }
-
+   @Getter
+   @Setter
+    public class Error{
+        private String error;
+        private LocalDateTime  date ;
+    }
     @GetMapping(value = "/ws/personaje/list")
-    public List<Character> listCharacters (@RequestParam("page") int page ,
-                                           @RequestParam("sort_attr") String sort_attr ,
-                                           @RequestParam("sort_type") String sort_attr,
-                                           @RequestParam("sort_attr") String sort_attr,
-
-
-
+    public ResponseEntity<HashMap<String, Object>> listCharacters (@RequestParam(value = "page",required = false,defaultValue ="1") Integer page ,
+                                                                   @RequestParam(value = "sort_attr", required = false, defaultValue="id") String sort_attr ,
+                                                                   @RequestParam(value = "sort_type",required = false, defaultValue = "asc") String sort_type,
+                                                                   @RequestParam(value = "limit", required = false, defaultValue = "20") Integer limit,
+                                                                   @RequestParam(value = "search_attr",required = false, defaultValue = "name") String search_attr,
+                                                                   @RequestParam(value = "search_text",required = false, defaultValue = "") String search_text
     ){
-        HashSet<Character> primerFiltro  = new HashSet<>();
-        primerFiltro= characterRepository.findByNameIgnoreCaseContaining('%'+busqueda+'%');
-        HashSet<Character> segundoFiltro  = new HashSet<>();
-        HashSet<Character> tercerFiltro  = new HashSet<>();
-        HashSet<Character> cuartoFiltro  = new HashSet<>();
-        HashSet<Character> quintoFiltro  = new HashSet<>();
-        HashSet<Character> sextoFiltro  = new HashSet<>();
+        HashMap<String, Object> responseJson = new HashMap<>();
+        HashMap<String, Object> pageable = new HashMap<>();
+        //Definimos valores por defecto
 
+        String error = limit>20  ? "error" : "";
 
-        return characterRepository.findAll();
+        if(error.equals("")){
+            List<Character> charac =  characterRepository.megaFiltrado(search_attr,'%'+search_text+'%',sort_attr,sort_type,limit,page*limit );
+            responseJson.put("content", charac);
+            responseJson.put("pageable", charac);
+            return ResponseEntity.ok(responseJson);
+        }else{
+            HashMap<String, Object> err = new HashMap<>();
+            err.put("error","limite invalido, debe ser menor a 20");
+            err.put("date",""+LocalDate.now());
+            return ResponseEntity.badRequest().body(err);
+        }
     }
     //PARA QUE JOSH TRABAJE :
+
+
+    @DeleteMapping(value = "/ws/personaje/delete/{id}")
+    public ResponseEntity<HashMap<String,Object>> borrar(@PathVariable("id") String idStr){
+        HashMap<String, Object> jsonResponse=new HashMap<>();
+        try{
+            Integer id=Integer.parseInt(idStr);
+            Optional<Character> optionalCharacter = characterRepository.findById(id);
+            if(optionalCharacter.isPresent()){
+                characterRepository.deleteById(id);
+                return ResponseEntity.ok(jsonResponse);
+            }else {
+                jsonResponse.put("error","ID personaje NO encontrado");
+                String horaActual= LocalDateTime.now().toString();
+                jsonResponse.put("date",horaActual);
+                return ResponseEntity.badRequest().body(jsonResponse);
+            }
+        }catch (NumberFormatException e){
+            jsonResponse.put("error","El ID no es un número");
+            String horaActual=LocalDateTime.now().toString();
+            jsonResponse.put("date",horaActual);
+            return ResponseEntity.badRequest().body(jsonResponse);
+        }
+    }
 
 }
